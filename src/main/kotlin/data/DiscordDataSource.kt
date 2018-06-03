889d97d38247466abb7b17d720da8f0f8074fa37
firezenk <firezenk@gmail.com>
2018-06-03 18:35:47 +0200

    Create a separate datasource to isolate Discord

--- src/main/kotlin/data/DiscordDataSource.kt
+++ src/main/kotlin/data/DiscordDataSource.kt
@@ -1,4 +1,26 @@
-class DiscordDataSource {
+package data
 
-    
+import domain.models.Event
+import io.reactivex.Flowable
+import io.reactivex.processors.PublishProcessor
+import org.javacord.api.DiscordApiBuilder
+
+class DiscordDataSource(token: String) {
+
+    private val api = DiscordApiBuilder().setToken(token).login().join()
+
+    private val publisher: PublishProcessor<Event> = PublishProcessor.create()
+    private val flowable = publisher.onBackpressureLatest()
+
+    fun listenMessages(): Flowable<Event> {
+        println("You can invite the bot by using the following url: " + api.createBotInvite())
+
+        api.addMessageCreateListener({ event ->
+            publisher.onNext(Event(event.messageId, event.message.content, {
+                event.channel.sendMessage(it)
+            }))
+        })
+
+        return flowable
+    }
 }
